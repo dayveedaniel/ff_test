17f2253735c53683fe3a5fcc14f285f5634d38da
dayveedaniel <47108741+dayveedaniel@users.noreply.github.com>
2021-05-13 09:18:55 +0700

    Update map_screen.dart

--- lib/src/screens/map_screen.dart
+++ lib/src/screens/map_screen.dart
@@ -1,15 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import '../bloc/news_bloc.dart';
-
 
 class MapScreen extends StatelessWidget {
 
-  NewsBloc newsBloc = NewsBloc();
-
   Widget build(context) {
-
-    newsBloc.getNews();
 
     return  Container(
         height: double.infinity,
